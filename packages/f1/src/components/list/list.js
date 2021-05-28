05a73843a71696e2d47ab56d14c8caa16176a173
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2021-05-28 15:10:35 +0400

    Jobs page major fixes

--- packages/f1/src/components/list/list.js
+++ packages/f1/src/components/list/list.js
@@ -1,4 +1,4 @@
-//! MEANT TO ONLY DISPLAY POSTS FROM THE "products" CATEGORY
+// // ! MEANT TO ONLY DISPLAY POSTS FROM THE "products" CATEGORY
 
 import React from "react";
 import { connect, styled, decode } from "frontity";
@@ -6,75 +6,104 @@ import Item from "./list-item";
 import Pagination from "./pagination";
 
 const List = ({ state }) => {
-  // Get the data of the current list.
-  const data = state.source.get(state.router.link);
-  let title = "Products";
-  if (data.isAwsmJobOpeningsArchive) {
-    title = "Career";
-  }
+    // Get the data of the current list.
+    const data = state.source.get(state.router.link);
+    let title = "Products";
+    if (data.isAwsmJobOpeningsArchive) {
+        title = "Jobs";
+    }
 
-  return (
-    <Container>
-      {/* If the list is a blog posts, we render a title. */}
-      {data.isPostArchive && <Header>{title}</Header>}
-      {/* If the list is a AWSM Job career page, we render a title. */}
-      {data.isAwsmJobOpeningsArchive && <Header>{title}</Header>}
-      {/* If the list is a taxonomy, we render a title. */}
-      {data.isTaxonomy && (
-        <Header>
-          {data.taxonomy}:{" "}
-          <b>{decode(state.source[data.taxonomy][data.id].name)}</b>
-        </Header>
-      )}
+    return (
+        <Container>
+            {/* If the list is a blog posts, we render a title. */}
+            {data.isPostArchive && <Header>{title}</Header>}
+            {/* If the list is a AWSM Job career page, we render a title. */}
+            {data.isAwsmJobOpeningsArchive && <Header>{title}</Header>}
+            {/* If the list is a taxonomy, we render a title. */}
+            {data.isTaxonomy && (
+                <Header>
+                    {data.taxonomy}:{" "}
+                    <b>{decode(state.source[data.taxonomy][data.id].name)}</b>
+                </Header>
+            )}
 
-      {/* If the list is for a specific author, we render a title. */}
-      {data.isAuthor && (
-        <Header>
-          Author: <b>{decode(state.source.author[data.id].name)}</b>
-        </Header>
-      )}
+            {/* If the list is for a specific author, we render a title. */}
+            {data.isAuthor && (
+                <Header>
+                    Author: <b>{decode(state.source.author[data.id].name)}</b>
+                </Header>
+            )}
 
-      {/* Iterate over the items of the list. */}
-      {data.isAwsmJobOpeningsArchive && (
-        <section className="section job-listing">
-          <div className="container">
-            <div className="row">
-              {/* Iterate over the items of the list. */}
-              {data.items.map(({ type, id }) => {
-                const item = state.source[type][id];
-                // Render one Item component for each one.
-                return <Item key={item.id} item={item} />;
-              })}
-            </div>
-          </div>
-        </section>
-      )}
-      {!data.isAwsmJobOpeningsArchive && (
-        <>
-          {data.items.map(({ type, id }) => {
-            const item = state.source[type][id];
-            // Render one Item component for each one.
-            return <Item key={item.id} item={item} />;
-          })}
-        </>
-      )}
+            {/* If the list is a AWSM Job career page, we render the introductory job posting text. */}
+            {data.isAwsmJobOpeningsArchive && (
+                <section className="section">
+                    <h3>
+                        We are always interested in hearing from talented
+                        people.
+                    </h3>
+                    <br />
+                    <p>
+                        {" "}
+                        You can send your application to us, with a suitable
+                        covering letter/email and a detailed CV by email to{" "}
+                        <a href="mailto:info@tarainstruments.com">
+                            info@tarainstruments.com
+                        </a>
+                        .{" "}
+                    </p>
+                    <p>
+                        Please make it clear in your application the type of
+                        role which interests you.{" "}
+                    </p>
+                    <p>
+                        We aim to contact individuals who have applied
+                        speculatively when a suitable role arises, but please
+                        don’t hesitate to apply again at a later date if you see
+                        a role you think is right for you.
+                    </p>
+                </section>
+            )}
+            {/* Iterate over the items of the list. */}
+            {data.isAwsmJobOpeningsArchive && (
+                <section className="section job-listing">
+                    <div className="container">
+                        <div className="row">
+                            {/* Iterate over the items of the list. */}
+                            {data.items.map(({ type, id }) => {
+                                const item = state.source[type][id];
+                                // Render one Item component for each one.
+                                return <Item key={item.id} item={item} />;
+                            })}
+                        </div>
+                    </div>
+                </section>
+            )}
+            {!data.isAwsmJobOpeningsArchive && (
+                <>
+                    {data.items.map(({ type, id }) => {
+                        const item = state.source[type][id];
+                        // Render one Item component for each one.
+                        return <Item key={item.id} item={item} />;
+                    })}
+                </>
+            )}
 
-      <Pagination />
-    </Container>
-  );
+            <Pagination />
+        </Container>
+    );
 };
 
 export default connect(List);
 
 const Container = styled.section`
-  width: 1200px;
-  margin: 0 auto;
-  padding-right: 15px;
-  padding-left: 15px;
-  list-style: none;
+    width: 1200px;
+    margin: 0 auto;
+    padding-right: 15px;
+    padding-left: 15px;
+    list-style: none;
 `;
 
 const Header = styled.h1`
-  text-align: center;
-  margin-bottom: 3rem;
+    text-align: center;
+    margin-bottom: 3rem;
 `;
