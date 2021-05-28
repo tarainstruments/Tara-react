227d15533a022e5241607dcd0f1e7e1a713e32d3
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2021-05-28 15:10:51 +0400

    Renamed project

--- frontity.settings.js
+++ frontity.settings.js
@@ -1,58 +1,62 @@
 const settings = {
-  name: "f1",
-  state: {
-    frontity: {
-      url: "https://tarainstruments.com",
-      title: "AWSM F1 - Frontity theme",
-      description: "A simple theme for Frontity WordPress development",
+    name: "tara-react",
+    state: {
+        frontity: {
+            url: "https://tarainstruments.com",
+            title: "AWSM F1 - Frontity theme",
+            description: "A simple theme for Frontity WordPress development"
+        }
     },
-  },
-  packages: [
-    {
-      name: "@awsmin/f1",
-      state: {
-        theme: {
-          menu: [
-            ["Home", "/"],
-            ["About", "/about/"],
-            ["Principals", "/principals/"],
-            ["Products", "/products/"],
-            ["Solutions", "/solutions/"],
-            ["Support", "/services/"],
-            ["News", "/news/"],
-            ["Jobs", "/jobs"],
-          ],
-          socialLinks: [
-            ["Facebook", "https://facebook.com/tarainstruments"],
-            ["Twitter", "https://twitter.com/tarainstruments"],
-          ],
-          featured: {
-            showOnList: true,
-            showOnPost: true,
-          },
-          // Whether to auto-fetch links on a page. Values can be "no" | "all" | "in-view" | "hover"
-          autoPrefetch: "hover",
+    packages: [
+        {
+            name: "@awsmin/f1",
+            state: {
+                theme: {
+                    menu: [
+                        ["Home", "/"],
+                        ["About", "/about/"],
+                        ["Principals", "/principals/"],
+                        ["Products", "/products/"],
+                        ["Solutions", "/solutions/"],
+                        ["Support", "/services/"],
+                        ["News", "/news/"],
+                        ["Jobs", "/jobs/"]
+                    ],
+                    socialLinks: [
+                        ["Facebook", "https://facebook.com/tarainstruments"],
+                        ["Twitter", "https://twitter.com/tarainstruments"],
+                        [
+                            "LinkedIn",
+                            "https://www.linkedin.com/company/tara-international/"
+                        ]
+                    ],
+                    featured: {
+                        showOnList: true,
+                        showOnPost: true
+                    },
+                    // Whether to auto-fetch links on a page. Values can be "no" | "all" | "in-view" | "hover"
+                    autoPrefetch: "hover"
+                }
+            }
         },
-      },
-    },
-    {
-      name: "@frontity/wp-source",
-      state: {
-        source: {
-          url: "https://tarainstruments.com",
-          homepage: "/home",
-          postsPage: "/products",
-          params: {
-            per_page: 50,
-          },
+        {
+            name: "@frontity/wp-source",
+            state: {
+                source: {
+                    url: "https://tarainstruments.com",
+                    homepage: "/home",
+                    postsPage: "/products",
+                    params: {
+                        per_page: 50
+                    }
+                }
+            }
         },
-      },
-    },
-    "@frontity/tiny-router",
-    "@frontity/html2react",
-    "@aamodtgroup/frontity-contact-form-7",
-    "@awsmin/frontity-wp-job-openings",
-  ],
+        "@frontity/tiny-router",
+        "@frontity/html2react",
+        "@aamodtgroup/frontity-contact-form-7",
+        "@awsmin/frontity-wp-job-openings"
+    ]
 };
 
 export default settings;
