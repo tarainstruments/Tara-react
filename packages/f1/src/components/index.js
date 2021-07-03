e2c2e85efd4b63c8bddc95fd6e0b838cb1d34b53
Rishikesh Rachchh <rishikeshrachchh@gmail.com>
2021-07-03 12:15:44 +0400

    Favicon markup in index.js

--- packages/f1/src/components/index.js
+++ packages/f1/src/components/index.js
@@ -27,6 +27,43 @@ const Theme = ({ state }) => {
       <Title />
       <Head>
         <html lang="en" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/wp-content/uploads/fbrfg/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/wp-content/uploads/fbrfg/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/wp-content/uploads/fbrfg/favicon-16x16.png"
+        />
+        <link
+          rel="manifest"
+          href="/wp-content/uploads/fbrfg/site.webmanifest"
+        />
+        <link
+          rel="mask-icon"
+          href="/wp-content/uploads/fbrfg/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link
+          rel="shortcut icon"
+          href="/wp-content/uploads/fbrfg/favicon.ico"
+        />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta
+          name="msapplication-config"
+          content="/wp-content/uploads/fbrfg/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
 
       {/* Add some global styles for the whole site, like body or a's. 
